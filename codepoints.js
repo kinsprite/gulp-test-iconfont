36c3d183527d61b079a72080e77e0501ad6891de
kinsprite <qxqs11@126.com>
2017-10-18 00:49:45 +0800

    使用新的方法生成字体图标CSS.

--- codepoints.js
+++
@@ -1,8 +0,0 @@
-/**
- * file name to unicode number
- */
-module.exports = {
-    // 'my-filename': 0xE000
-    add: 0xE010,
-    application: 0xE011,
-};
--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,92 @@
-var gulp = require('gulp');
-var iconfont = require('gulp-iconfont');
-var iconfontCss = require('gulp-iconfont-css');
-var template = require("gulp-template");
-var del = require('del');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const iconfont = require('gulp-iconfont');
+const consolidate = require('gulp-consolidate');
+const bs = require('browser-sync').create();
+const del = require('del');
 
-var fs = require("fs");
-var codepoints = require('./codepoints');
+/**
+ * Font settings
+ */
+const fontName = 'netstar-icons'; // set name of your symbol font
+const className = 'nsicon'; // set class name in your CSS
+const template = 'fontawesome-style'; // or 'foundation-style'
+const skethcFileName = 'symbol-font-14px.sketch'; // or 'symbol-font-16px.sketch'
 
-var icons = fs.readdirSync("src/icons");
+/**
+ * Recommended to get consistent builds when watching files
+ * See https://github.com/nfroidure/gulp-iconfont
+ */
+const timestamp = Math.round(Date.now() / 1000);
 
-icons = icons.map(function (icon) {
-  return icon.replace(/\.\w+$/, '');
-});
-
-// 定义字体名称、CSS Class名称
-var fontName = 'netstar-icons';
-// 定义CSS Class名称
-var className = 'nsicon';
-
-function checkUniqueCodePoints(codepoints) {
-  var cp2fileName = {};
-  var cp;
-
-  for (var fileName in codepoints) {
-    cp = codepoints[fileName];
-
-    if (cp2fileName[cp] !== undefined) {
-      throw new Error('Duplicate codepoint "0x' + cp.toString(16) + '".');
-    }
-
-    cp2fileName[cp] = fileName;
-  }
-}
-
-function checkAllFileCodePoints(codepoints) {
-  icons.forEach(function (icon) {
-    if (!codepoints[icon]) {
-      throw new Error('No codepoint for "' + icon + '".');
-    }
-  });
-}
-
-gulp.task('iconfont', function () {
-  checkUniqueCodePoints(codepoints);
-  checkAllFileCodePoints(codepoints);
-
-  return gulp.src('src/icons/*.svg')
-    .pipe(iconfontCss({
-      cssClass: className,
-      fontName: fontName,
-      fixedCodepoints: codepoints,
-      path: 'src/templates/_icons.css',
-      targetPath: '../../dist/css/_icons.css',
-      fontPath: '../fonts/'
-    }))
+gulp.task('iconfont', () =>
+  gulp.src('src/icons/*.svg')
     .pipe(iconfont({
-      fontName: fontName,
-      formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
+      fontName,
+      formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
       normalize: true,
       fontHeight: 1001,
-      // prependUnicode: false
+      prependUnicode: true,
+      timestamp,
+      log: () => {} // suppress unnecessary logging
     }))
-    .pipe(gulp.dest('dist/fonts/'));
-});
+    .on('glyphs', (glyphs) => {
+      const options = {
+        className,
+        fontName,
+        fontPath: '../fonts/', // set path to font (from your CSS file if relative)
+        glyphs: glyphs.map(mapGlyphs)
+      };
 
-gulp.task('example', function () {
-  return gulp.src('src/example/index.html')
-    .pipe(template({
-      icons: icons,
-      cssClass: className
-    }))
-    .pipe(gulp.dest("./dist/example"));
-});
+      gulp.src(`templates/${template}.css`)
+        .pipe(consolidate('lodash', options))
+        .pipe(rename({ basename: fontName }))
+        .pipe(gulp.dest('dist/css/')); // set path to export your CSS
+
+      // if you don't need sample.html, remove next 4 lines
+      gulp.src(`templates/${template}.html`)
+        .pipe(consolidate('lodash', options))
+        .pipe(rename({ basename: 'sample' }))
+        .pipe(gulp.dest('dist/')); // set path to export your sample HTML
+    })
+    .pipe(gulp.dest('dist/fonts/')) // set path to export your fonts
+);
+
+gulp.task('watch', () => {
+  bs.init({
+    files: 'dist/sample.html',
+    server: 'dist/',
+    startPath: '/sample.html',
+    middleware: cacheControl
+  });
+
+  gulp.watch('src/icons/*.svg', gulp.series('iconfont', (cb) => {
+    bs.reload();
+    cb();
+  }));
+})
 
 gulp.task('clean', function () {
   return del(['./dist']);
 });
 
-gulp.task('default', gulp.series('clean', 'iconfont', 'example'));
+gulp.task('default', gulp.series('clean', 'iconfont'));
+
+/**
+ * This is needed for mapping glyphs and codepoints.
+ */
+function mapGlyphs (glyph) {
+  if (glyph.name.indexOf(' ') !== -1) {
+    throw new Error(`Invalid glyph file name: "${glyph.name}", blank space isn't allowed!`);
+  }
+
+  return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) };
+}
+
+/**
+ * This keeps browser from caching fonts for your testing environment
+ */
+function cacheControl (req, res, next) {
+  res.setHeader('Cache-control', 'no-store');
+  next();
+}
